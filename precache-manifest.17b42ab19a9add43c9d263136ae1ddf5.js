126cc3ed8d7008ddf49bcc0327baad22b7af3428
M.Emmad Saiq <57301519+EmmadSadiq01@users.noreply.github.com>
2020-11-13 23:47:55 +0500

    Add files via upload

---
+++ precache-manifest.17b42ab19a9add43c9d263136ae1ddf5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963f1da1f9ff669ae43e5bed6c70f154",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59dcf303843f428b285c",
+    "url": "/static/css/2.3fa4a4fe.chunk.css"
+  },
+  {
+    "revision": "1cbf7196aea8796c5f49",
+    "url": "/static/css/main.177d2d3c.chunk.css"
+  },
+  {
+    "revision": "59dcf303843f428b285c",
+    "url": "/static/js/2.9986782a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9986782a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cbf7196aea8796c5f49",
+    "url": "/static/js/main.8b202570.chunk.js"
+  },
+  {
+    "revision": "7a1c4ee6b2728ac05991",
+    "url": "/static/js/runtime-main.f65ea6a2.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
